e7bae67d4db7975124675ff305850ff02b35eaf7
Aleksey Karpenko <akarpenko@cogniance.com>
2019-05-29 18:03:28 +0300

    cancel group of requests

--- index.js
+++ index.js
@@ -16,10 +16,16 @@ export default function patchAxios(axios, options) {
    * Any request with a `requestId` key in the config will be:
    *  - cancelled if already sent
    *  - added to the `pendingRequests` hash if not, with a cancel function
+   * Any request with a `requestGroup` key in config will be:
+   *  - added to the corresponding group of `pendingRequests` with a cancel function
    */
   axios.interceptors.request.use((config) => {
-    const { requestId } = config;
-    if (requestId) {
+    const { requestId, requestGroup } = config;
+    if (requestGroup) {
+      const source = CancelToken.source();
+      config.cancelToken = source.token;
+      config.requestGroupIndex = requestManager.addGroupRequest(requestGroup, source.cancel);
+    } else if (requestId) {
       const source = CancelToken.source();
       config.cancelToken = source.token;
       requestManager.addRequest(requestId, source.cancel);
@@ -29,19 +35,22 @@ export default function patchAxios(axios, options) {
 
   /**
    * Global response interceptor
-   * Check for the `requestId` and remove it from the `pendingRequests` hash
+   * Check for the `requestId` or `requestGroup` and remove it from the `pendingRequests` hash (or it's `requestGroup` sub hash)
    */
   axios.interceptors.response.use((response) => {
-    const { requestId } = response.config;
+    const { requestId, requestGroup, requestGroupIndex } = response.config;
     if (requestId) {
       requestManager.removeRequest(requestId);
+    }
+    if (requestGroup && requestGroupIndex) {
+      requestManager.removeGroupRequest(requestGroup, requestGroupIndex);
     }
     return response;
   });
 
   /**
-   * Global axios method to cancel a single request by ID
-   * @param requestId: string
+   * Global axios method to cancel a single request by ID or the whole group by it's name
+   * @param requestId: string Single request ID or group name
    * @param reason
    */
   axios.cancel = (requestId, reason) => {
